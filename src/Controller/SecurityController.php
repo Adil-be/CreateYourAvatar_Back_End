dc5ee2e11e8389831412d3d151e971211cd952a9
Adel Benyahia <a.benyahia6993@gmail.com>
2023-08-18 16:51:44 +0200

    Api plateform post config

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -28,5 +29,11 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/api/login', name: 'security_api_login')]
+    public function apiLogin(): Response
+    {
+        throw new LogicException('This method can be blank - it will be intercepted by the longin key on your firewall.');
     }
 }
